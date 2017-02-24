42f89bfd3ac0ea4fefc5a89728832220d53294d9
Evgeny Gusev <eugene.gusev@team.wrike.com>
2017-02-25 01:41:37 +0300

    simplify

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
 const dbUrl = process.env.MONGO_URL;
 
 let db;
+
 const USER_COOKIE_NAME = 'ng2-poll-cookie';
 
 app.use(bodyParser.json());
@@ -19,8 +21,8 @@ let logger = morgan(function(tokens, req, res) {
         tokens.method(req, res),
         tokens.url(req, res),
         tokens.status(req, res),
-        req.cookies[USER_COOKIE_NAME] || 'unknown', // TODO use chalk
-        req.body.vote || 'WRONG_VOTE',
+        _getCookie(req) || 'unknown', // TODO use chalk
+        req.body.vote || 'NONE',
         tokens['response-time'](req, res), 'ms',
     ].join(' ');
 });
@@ -29,36 +31,65 @@ app.use(logger);
 app.use(express.static(__dirname + '/public'));
 
 app.post('/vote', (req, res) => {
-    let fingerprint = req.headers['fingerprint'];
+    let fingerprint = _getFingerprint(req);
     if(!fingerprint) {
         res.sendStatus(400);
         return;
     }
 
-    let cookieUserId = req.cookies[USER_COOKIE_NAME];
-    let user = cookieUserId || fingerprint;
+    let cookieUserId = _getCookie(req);
 
-    let query = {};
-    if(cookieUserId) {
-        query = {user: cookieUserId};
-    }
     let data = {
-        user: user,
+        fingerprint: fingerprint,
         vote: req.body.vote,
     };
 
-    db.collection('votes').findOneAndUpdate(query, data, {upsert: true})
-        .then((result) => {
-            if(result.lastErrorObject.upserted) {
-                res.cookie(USER_COOKIE_NAME, fingerprint, {
-                    maxAge: 30 * 24 * 60 * 60 * 1000,
-                    httpOnly: true,
-                    secure: false,
-                });
-            }
-            res.sendStatus(200);
-    });
+    if(!cookieUserId) {
+        insertNewVote(data, res);
+    } else {
+        updateVote(cookieUserId, data, res);
+    }
 });
+
+app.get('/vote', (req, res) => {
+    let cookieUserId = _getCookie(req);
+    if(!cookieUserId) {
+        res.sendStatus(401);
+        return;
+    }
+    db.collection('votes').findOne({user: cookieUserId})
+        .then((result) => {
+            res.send({vote: result.vote});
+        });
+});
+
+function _getCookie(req) {
+    return req.cookies['ng2-poll-cookie'];
+}
+
+function _getFingerprint(req) {
+    return req.headers['fingerprint'];
+}
+
+function insertNewVote(data, res) {
+    db.collection('votes').insertOne(data)
+        .then((result) => {
+            res.cookie(USER_COOKIE_NAME, JSON.stringify(result.insertedId), {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+                secure: false,
+            });
+            res.sendStatus(200);
+        });
+}
+
+function updateVote(cookieUserId, data, res) {
+    let id = new ObjectID(JSON.parse(cookieUserId));
+    db.collection('votes').updateOne({_id: id}, data)
+        .then(() => {
+            res.sendStatus(200);
+        });
+}
 
 MongoClient.connect(dbUrl, (err, database) => {
     if (err) {
